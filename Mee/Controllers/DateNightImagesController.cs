9d3ed0258ee421f8c74a2ce3a563761a00fb25ae
MrMencia <Avimencia@gmail.com>
2019-11-14 11:15:30 -0600

    added view model for datenightImages

---
+++ Mee/Controllers/DateNightImagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mee.Controllers
+{
+    public class DateNightImagesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
